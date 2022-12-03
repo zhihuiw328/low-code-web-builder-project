b5bc70c4d96d86094f89abd350eb65c627df133c
Chen Song <chens3@illinois.edu>
2022-12-02 18:32:25 -0600

    update edit page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
+import {Routes, Route, Outlet, Link} from 'react-router-dom';
+import Edit from './edit';
+import MainPage from "./main"
 
-function App() {
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  return(
+      <div>
+          <Routes>
+              <Route path="/" element={<Layout/>}>
+              <Route index element={<MainPage/> } />
+              <Route path="Edit" element={<Edit/> } />
+              <Route path= "*" element={<Bad count={count} setCount ={setCount}/>} />
+              </Route>
+          </Routes>
+      </div>
+  )
+};
+
+function Layout() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+          <Link to ="/">Home</Link>
+          <Outlet/>
+      </div>
+    );
 }
 
-export default App;
+function Bad(props) {
+  return (
+      <div className ="body">
+        <h2>There is no info for this page, but do you know {props.count} people has already liked our website !</h2>
+        <h2>Click the button to be one of them!</h2>
+        <button onClick = {()=>props.setCount(props.count + 1)}>I Like this page</button>
+        <p>
+          <Link to="/"><h2>Go to the home page</h2></Link>
+        </p>
+      </div>
+    );
+};
