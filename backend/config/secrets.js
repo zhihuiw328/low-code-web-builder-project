806519543d5c219b2f26f932f24f958223571b53
zhihuiw <zhihuiw@illinois.edu>
2022-12-05 21:52:49 -0600

    connect mongo database

---
+++ backend/config/secrets.js
@@ -0,0 +1,5 @@
+module.exports = {
+    token : "secret-starter-mern",
+    mongo_connection : "mongodb+srv://zhihuiw:123126@cluster0.9ya5gzh.mongodb.net/?retryWrites=true&w=majority"
+    //example: mongo_connection : "mongodb+srv://[type-yours]:[type-yours]@[type-yours-web-provided].mongodb.net/test?retryWrites=true"
+};
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ const express = require('express')
 const dotenv = require('dotenv')
 const notFound = require('./middlewares/errorMiddleware')
 const errorHandler = require('./middlewares/errorMiddleware')
-
+const mongoose = require('mongoose')
 const userRoutes = require('./routes/userRoutes')
+const secrets = require('./config/secrets')
+
 
 dotenv.config()
 
@@ -18,5 +20,9 @@ app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
+
+// Connect to a MongoDB --> Uncomment this once you have a connection string!!
+mongoose.connect(secrets.mongo_connection,  { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`))
