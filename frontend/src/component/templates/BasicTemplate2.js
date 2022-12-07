0775aedf87305e21361b0e4c938494c874d015a3
Lilac (Yinmeng) Lai <78175838+lilacyl@users.noreply.github.com>
2022-12-06 19:27:10 -0600

    able to take input from user

--- frontend/src/component/templates/BasicTemplate2.js
+++ frontend/src/component/templates/BasicTemplate2.js
@@ -1,21 +1,26 @@
 import React, { useState } from 'react';
 
-function BasicTemplate2() {
-  const greeting = 'Hello Function Component!';
+function BasicTemplate2(props) {
+    const userInput = props.userInput;
+    // const userInput = {
+    //     pageTitle : "USER INPUT PAGE TITLE",
+    //     text : "USER INPUT TEXT",
+    // }
+    console.log(userInput);
 
-  return (
-    <>
-    <div>
-        <h1>HELLO WORLD</h1>
+    return (
+        <>
+        <div>
+        <h1>HELLO WORLD {userInput.pageTitle}</h1>
         <p>My first paragraph</p>
         <h2>Subeading 1</h2>
-        <p>My first paragraph</p>
+        <p>My first paragraph {userInput.text}</p>
         <h2>Subeading 1</h2>
         <p>My first paragraph</p>
-    </div>
-    
-    </>
-  );
+        </div>
+
+        </>
+    );
 }
 
 export default BasicTemplate2;
