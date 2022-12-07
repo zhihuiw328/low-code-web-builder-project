6972410353fedb65b98202e243b43c0c8d109511
Lilac (Yinmeng) Lai <78175838+lilacyl@users.noreply.github.com>
2022-12-06 18:50:49 -0600

    added basic template file

---
+++ frontend/src/component/templates/BasicTemplate1.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+function BasicTemplate1() {
+  const greeting = 'Hello Function Component!';
+
+  return (
+    <>
+    <div>
+        <h1>HELLO WORLD</h1>
+        <p>My first paragraph</p>
+        <h2>Subeading 1</h2>
+        <p>My first paragraph</p>
+        <h2>Subeading 1</h2>
+        <p>My first paragraph</p>
+    </div>
+    
+    </>
+  );
+}
+
+export default App;
