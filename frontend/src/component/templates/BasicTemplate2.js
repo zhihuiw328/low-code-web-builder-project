d18cb0f4f1ef1ca1d4e4ae2ce01e5f9344364a68
Lilac (Yinmeng) Lai <78175838+lilacyl@users.noreply.github.com>
2022-12-07 21:29:08 -0600

    color change working

--- frontend/src/component/templates/BasicTemplate2.js
+++ frontend/src/component/templates/BasicTemplate2.js
@@ -13,7 +13,7 @@ class BasicTemplate2 extends React.Component {
                     name: "pageTitle",
                     text: "USER INPUT PAGE TITLE",
                     style: "div_hover",
-                    textColor: "black",
+                    color: "black",
                     backgroundColor: "white"
 
                 },
@@ -21,7 +21,7 @@ class BasicTemplate2 extends React.Component {
                     name: "titleText",
                     text: "TITLE TEXT",
                     style: "div_hover",
-                    textColor: "black",
+                    color: "black",
                     backgroundColor: "white"
 
                 },
@@ -29,7 +29,7 @@ class BasicTemplate2 extends React.Component {
                     name: "subTitle1",
                     text:"SUB TITLE 1",
                     style: "div_hover",
-                    textColor: "black",
+                    color: "black",
                     backgroundColor: "white"
 
                 },
@@ -37,7 +37,7 @@ class BasicTemplate2 extends React.Component {
                     name: "subText1",
                     text:"SUB TITLE TEXT 1",
                     style: "div_hover",
-                    textColor: "black",
+                    color: "black",
                     backgroundColor: "white"
 
                 },
@@ -45,7 +45,7 @@ class BasicTemplate2 extends React.Component {
                     name: "subTitle2",
                     text:"SUB TITLE TEXT 2",
                     style: "div_hover",
-                    textColor: "black",
+                    color: "black",
                     backgroundColor: "white"
 
                 },
@@ -53,7 +53,7 @@ class BasicTemplate2 extends React.Component {
                     name: "subText2",
                     text:"SUBTEXT TEXT 2",
                     style: "div_hover",
-                    textColor: "black",
+                    color: "black",
                     backgroundColor: "white"
 
                 }
@@ -135,12 +135,15 @@ class BasicTemplate2 extends React.Component {
             this.setState({currentSection:null})
         } else {
             this.updateClickedSection(section)
-            // console.log(this.state.pageContent[section].style)
+            this.passTextToParent(this.state.pageContent[section].text)
+            this.passTextColorToParent(this.state.pageContent[section].color)
+            this.passBackgroundColorToParent(this.state.pageContent[section].backgroundColor)
         }
     };
     
     updateSectionInfo = (section, newText, newColor, newBackgroundColor) => {
-        console.log(newText)
+        console.log(12344)
+        console.log(newColor)
         this.setState(prevState => {
             let newPageContent = Object.assign({}, prevState.pageContent)
             newPageContent[section] = {
@@ -162,6 +165,8 @@ class BasicTemplate2 extends React.Component {
                     this.state.pageContent[this.state.currentSection].backgroundColor)
             }
             if (previousProps.color !== this.props.color) {
+                console.log(this.props.color)
+                console.log(1)
                 this.updateSectionInfo(this.state.currentSection,
                     this.state.pageContent[this.state.currentSection].text,
                     this.props.color,
@@ -182,27 +187,44 @@ class BasicTemplate2 extends React.Component {
             
             <>
             <div id='container'>
-                <div onClick={(e) => this.handleSectionClick(e, "pageTitle")} className={this.state.pageContent.pageTitle.style}  style={{textAlign:"center", backgroundColor: "pink"}}>
+                <div onClick={(e) => this.handleSectionClick(e, "pageTitle")} 
+                    className={this.state.pageContent.pageTitle.style}
+                    style={{textAlign:"center", 
+                        backgroundColor: this.state.pageContent.pageTitle.backgroundColor,
+                        color: this.state.pageContent.pageTitle.color,
+                        margin:0}}>
                     <h1>{this.state.pageContent.pageTitle.text}</h1>
                 </div>
 
-                <div onClick={(e) => this.handleSectionClick(e, "titleText")} className={this.state.pageContent.titleText.style}>
+                <div onClick={(e) => this.handleSectionClick(e, "titleText")}
+                    className={this.state.pageContent.titleText.style}
+                    style={{backgroundColor: this.state.pageContent.titleText.backgroundColor, 
+                        color: this.state.pageContent.titleText.color,
+                        margin:0}}>
                     <p>{this.state.pageContent.titleText.text}</p>
                 </div>
 
-                <div onClick={(e) => this.handleSectionClick(e, "subTitle1")} className={this.state.pageContent.subTitle1.style}>
+                <div onClick={(e) => this.handleSectionClick(e, "subTitle1")} 
+                    className={this.state.pageContent.subTitle1.style}
+                    style={{backgroundColor: this.state.pageContent.subTitle1.backgroundColor, color: this.state.pageContent.subTitle1.color}}>
                     <h2>{this.state.pageContent.subTitle1.text}</h2>
                 </div>
 
-                <div onClick={(e) => this.handleSectionClick(e, "subText1")} className={this.state.pageContent.subText1.style}>
+                <div onClick={(e) => this.handleSectionClick(e, "subText1")}
+                    className={this.state.pageContent.subText1.style}
+                    style={{backgroundColor: this.state.pageContent.subText1.backgroundColor, color: this.state.pageContent.subText1.color}}>
                     <p>{this.state.pageContent.subText1.text}</p>
                 </div>
 
-                <div onClick={(e) => this.handleSectionClick(e, "subTitle2")} className={this.state.pageContent.subTitle2.style}>
+                <div onClick={(e) => this.handleSectionClick(e, "subTitle2")} 
+                    className={this.state.pageContent.subTitle2.style}
+                    style={{backgroundColor: this.state.pageContent.subTitle2.backgroundColor, color: this.state.pageContent.subTitle2.color}}>
                     <h2>{this.state.pageContent.subTitle2.text}</h2>
                 </div>
 
-                <div onClick={(e) => this.handleSectionClick(e, "subText2")} className={this.state.pageContent.subText2.style}>
+                <div onClick={(e) => this.handleSectionClick(e, "subText2")} 
+                    className={this.state.pageContent.subText2.style}
+                    style={{backgroundColor: this.state.pageContent.subText2.backgroundColor, color: this.state.pageContent.subText2.color}}>
                     <p>{this.state.pageContent.subText2.text}</p>
                 </div>
             </div>
