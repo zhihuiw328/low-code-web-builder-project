84068f9bf2409f05cc16685965cfcd7850a921c1
zhihuiw <zhihuiw@illinois.edu>
2022-12-10 17:21:50 -0600

    swtich database

--- backend/config/secrets.js
+++ backend/config/secrets.js
@@ -1,5 +1,5 @@
 module.exports = {
     token : "secret-starter-mern",
-    mongo_connection : "mongodb+srv://zhihuiw:123126@cluster0.9ya5gzh.mongodb.net/final_project/?retryWrites=true&w=majority"
+    mongo_connection : "mongodb+srv://zhihuiw:123126@cluster0.9ya5gzh.mongodb.net/final_project?retryWrites=true&w=majority"
     //example: mongo_connection : "mongodb+srv://[type-yours]:[type-yours]@[type-yours-web-provided].mongodb.net/test?retryWrites=true"
 };
