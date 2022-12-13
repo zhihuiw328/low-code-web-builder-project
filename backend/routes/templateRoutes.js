336288a00d4412000a45203289e8769c6b39b298
yz96 <68858113+yangzho12138@users.noreply.github.com>
2022-12-12 18:48:04 -0600

    shared screen

--- backend/routes/templateRoutes.js
+++ backend/routes/templateRoutes.js
@@ -7,6 +7,7 @@ const router = express.Router()
 router.route('/').post(protect, templateControllers.CreateTemplate).get(protect, templateControllers.GetAllTemplate)
 // router.route('/login').post(userControllers.loginUser)
 router.route('/:id').get(protect, templateControllers.GetTemplate).delete(protect, templateControllers.deleteTemplate)
+router.route('/:id/visit').get(templateControllers.GetTemplate)
 
 
 module.exports = router
